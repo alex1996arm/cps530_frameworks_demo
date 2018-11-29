15f664c59edf652369be95a39c02154c1aaab1a5
Alexandre Roberge-Milanese <alex1996arm@gmail.com>
2018-11-29 17:36:50 -0500

    django files

---
+++ cps530_frameworks/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cps530_frameworks project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cps530_frameworks.settings')
+
+application = get_wsgi_application()
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alex1996arm/PycharmProjects/cps530_frameworks/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
